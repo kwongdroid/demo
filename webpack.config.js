cb898c5096fbd727a927df6f7afdb1dbf31c81de
kdroid27 <kwongdroid@qq.com>
2019-09-16 15:28:53 +0800

    getEntry

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
+function getEntry(projectName) {
+    let entry = {};
+    for (let name of projectName) {
+        entry[`${name}/main`] = `./src/${name}/main.js`
+    }
+    return entry
+}
+
+// 将projectName修改为自己的项目名称
+const projectName = [
+    'helloWorld'
+];
+
 const config = {
+    entry: getEntry(projectName)
 };
 
 module.exports = config
