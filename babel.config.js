2aeb7748d2b74e45e4715a9cd574b9711ec3793a
kdroid27 <kwongdroid@qq.com>
2019-09-17 16:45:48 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,7 @@ module.exports = function (api) {
                     chrome: '67',
                     safari: '11.1'
                 },
-                // env preset 只对我们所使用的并且目标浏览器中缺失的功能进行代码转换和加载 polyfill
+                // 按需加载 polyfill
                 // 如果我们不使用 env preset 的 "useBuiltIns" 参数（即设置为 "usage"），那么我们必须在所有代码之前通过 require 加载一次完整的 polyfill
                 // useBuiltIns: 'usage'
             }
