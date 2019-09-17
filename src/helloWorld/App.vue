c2506e9b4948a034aacb2aaa8685d79a6e2d5ccd
kdroid27 <kwongdroid@qq.com>
2019-09-17 16:05:49 +0800

    新增helloWorld项目

---
+++ src/helloWorld/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{message}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
